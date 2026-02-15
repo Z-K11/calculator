107926ff595a1fbfa62a2e556b65f449fd2b5b00
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-02-15 17:01:57 +0500

    Add functions for DMAS

---
+++ script.js
@@ -0,0 +1,17 @@
+function divide(a,b)
+{
+    return a/b;
+}
+function multiply(a,b)
+{
+    return a*b;
+}
+function sum(a,b)
+{
+    return a+b;
+}
+function subtract(a,b)
+{
+    return a-b;
+}
+
